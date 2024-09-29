6799bee2f63ca51a29d0daf44fcbd9f6aa20f99d
Alexandra18082 <alexandraserbulea3@gmail.com>
2024-09-29 18:54:27 +0300

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
-  //Cand se apasa pe buton
   $(".options-button").on("click", function () {
-    //on = event listener in jquery
     const answer = $(this).data("answer");
-    //mai sus am selectat atributul data answer
+
     if (answer === "correct") {
       $("#result").text("Correct!").css("color", "green");
     } else {
